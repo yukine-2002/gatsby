5561af96727629469e53ccf7f2d88a336ee99dc3
Bùi Thành Senpai <bthanh2701@gmail.com>
2021-08-16 00:02:22 +0700

    project-gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default  ({data}) => {
     <Seo title="Home" />
     <div>
       <h1>Yuki</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} posts.</h4>
       {
         data.allMarkdownRemark.edges.map( ({node}) => (
         
